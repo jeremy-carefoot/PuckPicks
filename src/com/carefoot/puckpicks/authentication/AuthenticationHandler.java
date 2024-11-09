55b70f4ebf2ca0a89ea4414dcd7484bfdbb201bc
Jeremy Carefoot <jeremycarefoot@gmail.com>
2024-11-09 14:14:59 -0700

    Halfway done logged in? function

--- src/com/carefoot/puckpicks/authentication/AuthenticationHandler.java
+++ src/com/carefoot/puckpicks/authentication/AuthenticationHandler.java
@@ -7,6 +7,9 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import com.carefoot.puckpicks.data.DataManager;
+import com.carefoot.puckpicks.data.paths.PPServerUrlPath;
+import com.carefoot.puckpicks.data.requests.URLRequest;
+import com.carefoot.puckpicks.data.requests.YahooOAuthRefreshRequest;
 import com.carefoot.puckpicks.data.requests.YahooUserInfoRequest;
 import com.carefoot.puckpicks.main.Log;
 
@@ -36,14 +39,34 @@ public class AuthenticationHandler {
 				
 			} catch (IOException | JSONException e) {
 				Log.log("Could not grab user info with access token; attempting refresh token", Log.INFO);
-			} catch (URISyntaxException e) {// indicates that Yahoo API has been updated 
+			} catch (URISyntaxException e) {// indicates some sort of internal error
 				Log.log("Invalid Yahoo URL provided! Please report this error", Log.ERROR);
 				return null;
 			} 
 		}
 		
 		/* Now if there is a stored refreshToken, we will try it */
-		// TODO finish this
+		
+		/* First, retrieve oauth information from PPServer */
+		JSONObject oauthInfo;
+		try {
+			oauthInfo = DataManager.submitRequest(new URLRequest(PPServerUrlPath.getBaseURL(), PPServerUrlPath.OAUTH_INFO.path()));
+		} catch (Exception e) {
+			Log.log("Could not communicate with PuckPicks server to retrieve OAuth information: " + e.getMessage(), Log.ERROR);
+			return null;
+		}
+		
+		/* Attempt to refresh token */
+		try {
+			JSONObject response = DataManager.submitRequest(new YahooOAuthRefreshRequest(
+					oauthInfo.getString("token_endpoint"), oauthInfo.getString("client_id"), tokens.getRefreshToken()));
+			tokens.setAuthToken(response.getString("access_token"));
+			tokens.setRefreshToken(response.getString("refresh_token"));
+		} catch (IOException | JSONException e) {
+			Log.log("Could not renew access token for refresh token; revalidation may be required", Log.INFO);
+		} catch (URISyntaxException e) {// indicates some sort of internal error
+			Log.log("Invalid Yahoo URL provided! Please report this error", Log.ERROR);
+		} 
 		return null;
 	}
 
